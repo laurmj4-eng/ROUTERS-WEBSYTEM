eb2982ce4db10df5399ac8425f19cbb01d31b06d
laurmj4-eng <laurmj4@gmail.com>
2026-08-12 12:57:25 +0800

    fix: render API errors as JSON (diagnostic + good practice)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,5 +27,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $e, \Illuminate\Http\Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'error' => $e->getMessage(),
+                    'class' => get_class($e),
+                    'file'  => $e->getFile().':'.$e->getLine(),
+                ], 500);
+            }
+        });
     })->create();
